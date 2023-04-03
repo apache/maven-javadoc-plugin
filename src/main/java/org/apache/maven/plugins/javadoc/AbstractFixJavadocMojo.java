1936ec0e6d49cb6b63b135f33367f6a1839281b1
jaymulani <jaymulani26@gmail.com>
2023-04-02 21:33:31 -0300

    Refactor: executeClirr() to runClirrCheck() as this method checks all the base condition.

--- src/main/java/org/apache/maven/plugins/javadoc/AbstractFixJavadocMojo.java
+++ src/main/java/org/apache/maven/plugins/javadoc/AbstractFixJavadocMojo.java
@@ -432,8 +432,9 @@ public abstract class AbstractFixJavadocMojo extends AbstractMojo {
         }
 
         // run clirr
+        // Refactored Method
         try {
-            executeClirr();
+            runClirrCheck();
         } catch (MavenInvocationException e) {
             if (getLog().isDebugEnabled()) {
                 getLog().error("MavenInvocationException: " + e.getMessage(), e);
@@ -631,7 +632,9 @@ public abstract class AbstractFixJavadocMojo extends AbstractMojo {
      *
      * @throws MavenInvocationException if any
      */
-    private void executeClirr() throws MavenInvocationException {
+    // Refactoring : Rename method form executeClirr() to runClirrCheck()
+    // because it checks the base condition if it satifies or not
+    private void runClirrCheck() throws MavenInvocationException {
         if (ignoreClirr) {
             getLog().info("Clirr is ignored.");
             return;
