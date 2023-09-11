24362d288bc70c658cdcced4cc0b60b648180dc7
Tamas Cservenak <tamas@cservenak.net>
2023-09-11 20:28:07 +0200

    [MJAVADOC-742] Fix resolution of docletArtifacts (#186)
    Co-authored-by: Slawomir Jaranowski <s.jaranowski@gmail.com>
    Co-authored-by: Konrad Windszus <kwin@apache.org>

--- src/main/java/org/apache/maven/plugins/javadoc/AbstractJavadocMojo.java
+++ src/main/java/org/apache/maven/plugins/javadoc/AbstractJavadocMojo.java
@@ -132,7 +132,6 @@ import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.ArtifactTypeRegistry;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.graph.DefaultDependencyNode;
 import org.eclipse.aether.graph.DependencyFilter;
 import org.eclipse.aether.resolution.ArtifactRequest;
 import org.eclipse.aether.resolution.ArtifactResolutionException;
@@ -3251,8 +3250,11 @@ public abstract class AbstractJavadocMojo extends AbstractMojo {
 
             DependencyFilter filter = new ScopeDependencyFilter(
                     Arrays.asList(Artifact.SCOPE_COMPILE, Artifact.SCOPE_PROVIDED), Collections.emptySet());
-            DependencyRequest req =
-                    new DependencyRequest(new DefaultDependencyNode(RepositoryUtils.toArtifact(artifact)), filter);
+            DependencyRequest req = new DependencyRequest(
+                    new CollectRequest(
+                            new org.eclipse.aether.graph.Dependency(RepositoryUtils.toArtifact(artifact), null),
+                            RepositoryUtils.toRepos(project.getRemoteArtifactRepositories())),
+                    filter);
             Iterable<ArtifactResult> deps =
                     repoSystem.resolveDependencies(repoSession, req).getArtifactResults();
             for (ArtifactResult a : deps) {
