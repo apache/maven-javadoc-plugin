b2008a7133f4d80cdfb49e40804251d6df3e38de
Michael Osipov <michaelo@apache.org>
2024-08-14 19:39:08 +0200

    Try

--- src/main/java/org/apache/maven/plugins/javadoc/AbstractJavadocMojo.java
+++ src/main/java/org/apache/maven/plugins/javadoc/AbstractJavadocMojo.java
@@ -3759,13 +3759,14 @@ public abstract class AbstractJavadocMojo extends AbstractMojo {
             if (location == null || location.isEmpty()) {
                 continue;
             }
-            if (isValidJavadocLink(location, false)) {
-                addArgIfNotEmpty(
-                        arguments,
-                        "-linkoffline",
-                        JavadocUtil.quotedPathArgument(url) + " " + JavadocUtil.quotedPathArgument(location),
-                        true);
+            if (validateLinks && !isValidJavadocLink(location, false)) {
+                continue;
             }
+            addArgIfNotEmpty(
+                    arguments,
+                    "-linkoffline",
+                    JavadocUtil.quotedPathArgument(url) + " " + JavadocUtil.quotedPathArgument(location),
+                    true);
         }
     }
 
@@ -5617,7 +5618,11 @@ public abstract class AbstractJavadocMojo extends AbstractMojo {
                 }
             }
 
-            if (url != null && isValidJavadocLink(url, detected)) {
+            if (url != null) {
+                if (validateLinks && !isValidJavadocLink(url, detected)) {
+                    continue;
+                }
+
                 getLog().debug("Added Javadoc link: " + url + " for " + artifact.getId());
 
                 dependenciesLinks.add(url);
@@ -5814,14 +5819,14 @@ public abstract class AbstractJavadocMojo extends AbstractMojo {
             }
 
             try {
-                if (JavadocUtil.isValidElementList(elementListUri.toURL(), settings, validateLinks)) {
+                if (JavadocUtil.isValidElementList(elementListUri.toURL(), settings)) {
                     return true;
                 }
             } catch (IOException e) {
                 // ignore this because it is optional
             }
 
-            if (JavadocUtil.isValidPackageList(packageListUri.toURL(), settings, validateLinks)) {
+            if (JavadocUtil.isValidPackageList(packageListUri.toURL(), settings)) {
                 return true;
             }
 
