00dcb7b1b82f9875843ecb59c68482b7709a2a65
Elliotte Rusty Harold <elharo@ibiblio.org>
2025-08-15 07:15:25 -0400

    typos

--- src/main/java/org/apache/maven/plugins/javadoc/AbstractJavadocMojo.java
+++ src/main/java/org/apache/maven/plugins/javadoc/AbstractJavadocMojo.java
@@ -1023,7 +1023,7 @@ public abstract class AbstractJavadocMojo extends AbstractMojo {
      *
      * <b>Notes</b>:
      * <ol>
-     * <li>This option is ignored if the plugin is run in offline mode (using the {@code <offline>}
+     * <li>This option is ignored if the plugin is run in offline mode using the {@code <offline>}
      * setting or specifying {@code -o, --offline} or {@code -Dmaven.javadoc.offline=true} on the
      * command line.</li>
      * <li>all given links should have a fetchable <code>/package-list</code> or <code>/element-list</code>
