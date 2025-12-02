d4bfe8707bdea22f86fe326a1f3bdf9f1bfcd811
Sandra Parsick <sparsick@users.noreply.github.com>
2025-12-02 12:46:19 +0000

    chore: remove junit 3 references (#1279)
    Signed-off-by: Sandra Parsick <sandra@parsick.dev>

--- src/it/projects/MJAVADOC-444/moduleB/src/test/java/com/mycompany/myapp/AppBTest.java
+++ src/it/projects/MJAVADOC-444/moduleB/src/test/java/com/mycompany/myapp/AppBTest.java
@@ -19,39 +19,6 @@ package com.mycompany.myapp;
  * under the License.
  */
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
 public class AppBTest
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppBTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppBTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
