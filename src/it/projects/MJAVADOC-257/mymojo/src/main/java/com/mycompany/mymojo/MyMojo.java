f175c95d19c3b3c75aee799a92f1312c9ead51b0
Vaclav Haisman <vhaisman@gmail.com>
2026-08-08 15:34:43 +0200

    Migrate legacy plugin Javadoc annotations
    Replace legacy Maven Plugin Tools Javadoc tags in the MJAVADOC-257 fixture with maven-plugin-annotations metadata. Add the provided annotation dependency using the filtered Plugin Tools version.

--- src/it/projects/MJAVADOC-257/mymojo/src/main/java/com/mycompany/mymojo/MyMojo.java
+++ src/it/projects/MJAVADOC-257/mymojo/src/main/java/com/mycompany/mymojo/MyMojo.java
@@ -21,6 +21,9 @@ package com.mycompany.mymojo;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -28,19 +31,15 @@ import java.io.IOException;
 
 /**
  * Goal which touches a timestamp file.
- *
- * @goal touch
- *
- * @phase process-sources
  */
+@Mojo(name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class MyMojo
     extends AbstractMojo
 {
     /**
      * Location of the file.
-     * @parameter expression="${project.build.directory}"
-     * @required
      */
+    @Parameter(defaultValue = "${project.build.directory}", required = true)
     private File outputDirectory;
 
     public void execute()
